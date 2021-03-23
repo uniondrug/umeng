3732317e13400735af2f3df06849e22b08a78808
黄亚雄 <huangyaxiong@uniondrug.com>
2021-03-23 18:59:55 +0800

    firt init

---
+++ src/notification/android/AndroidUnicast.php
@@ -0,0 +1,11 @@
+<?php
+namespace Uniondrug\Umen\Android;
+
+class AndroidUnicast extends AndroidNotification {
+	function __construct() {
+		parent::__construct();
+		$this->data["type"] = "unicast";
+		$this->data["device_tokens"] = NULL;
+	}
+
+}
